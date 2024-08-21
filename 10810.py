91bc36ed4f29f38c9444d74fb156971184a07465
chaewon <codnjs9075@gmail.com>
2024-08-21 22:32:59 +0900

    baekjoon10810찐

---
+++ 10810.py
@@ -0,0 +1,12 @@
+N, M = map(int, input().split())
+Nlist = [0 for t in range(N)] # 입력받은 숫자 크기만큼 0으로 리스트 채우기
+
+for u in range(M):
+    i, j, k = map(int, input().split())
+    for num in range(i-1,j):
+        Nlist[num] = k
+
+print(*Nlist)
+
+
+
